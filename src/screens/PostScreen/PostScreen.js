5a1cd06c9687b014bd20b9cc24739c58fe4a701b
mcam1010 <malikcameron95@gmail.com>
2020-08-08 20:16:00 -0700

    Adding proper styling to post screen

--- src/screens/PostScreen/PostScreen.js
+++ src/screens/PostScreen/PostScreen.js
@@ -15,50 +15,35 @@ export default function PostScreen({navigation}) {
                 <Ionicons name="md-arrow-back" size={24} color="black"></Ionicons>
             </TouchableOpacity>
             <TouchableOpacity>
-                     <Text style={{ fontWeight: "800"}}>Post</Text>
+                     <Text style={{ fontWeight: "900"}}>Post</Text>
                  </TouchableOpacity>
             </View>
-            <View style={styles.top}>
-                <TextInput
+            <View style={styles.header}>
+            <TouchableOpacity>
+              <View>
+              <Image
+        style={styles.top}
+        source={require('../../../assets/tempAvatar.jpg')}
+      />
+            </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+            <TextInput
              autoFocus={ true }
-             numberOfLines={ 2 }
-             style={{ height: 20,  borderColor: 'black', borderWidth: 2,flexDirection:"row-reverse" ,borderBottomWidth: 2}}
+             multiline={true}
+             numberOfLines={ 4 }
+             style={{ flex: 1}}
              placeholder="Want to share something?"
             ></TextInput>
-          </View>
+                 </TouchableOpacity>
+            </View>
+                
            
         <View style={styles.container}>
-          <View style={styles.middle} />
-          <View style={styles.bottom} />
+          
+         
         </View>
      </SafeAreaView>
       );
   }
 
-// export default function PostScreen({navigation}) {
-//     return(
-//         <SafeAreaView style={styles.container}>
-//             <View style={styles.header}>
-//                 <TouchableOpacity>
-//                     <Ionicons name="md-arrow-back" size={24} color="black"></Ionicons>
-//                 </TouchableOpacity>
-//                 <TouchableOpacity>
-//                     <Text style={{ fontWeight: "800"}}>Post</Text>
-//                 </TouchableOpacity>
-//             </View>
-//             <View styles={styles.inputontainer}>
-//                 <Image source={require("../../../assets/tempAvatar.jpg")} styles={styles.avatar} resizeMode='contain'></Image>
-//                 <TextInput
-//                     autoFocus={ true }
-//                     multiline={ true }
-//                     numberOfLines={ 12 }
-//                     style={{ height: 40, borderColor: 'grey', borderWidth: 1 }}
-//                     placeholder="Want to share something?"
-//                 ></TextInput>
-//             </View>
-//             <TouchableOpacity style={styles.photo}>
-//                 <Ionicons name="md-camera" size={32} color="grey"></Ionicons>
-//             </TouchableOpacity>
-//         </SafeAreaView>
-//     )
-//   }
---
+++ src/screens/PostScreen/styles.js
@@ -0,0 +1,43 @@
+import { preventAutoHide } from 'expo/build/launch/SplashScreen';
+import { StyleSheet, ImageBackground } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "space-between",
+      padding: 19,
+      margin: 10,
+    },
+   
+    header:{
+         flexDirection: "row",
+         justifyContent: "space-between",
+          paddingHorizontal: 12,
+          paddingVertical: 5,
+          borderBottomWidth: 1,
+          borderBottomColor: "#D8D9DB"
+    },
+    top: {
+        flexDirection: "row",
+        alignSelf: "flex-start",
+        width: 50,
+        height: 50,
+        borderRadius: 100/2,
+        
+      
+    },
+    
+    middle: {
+      flex: 0.3,
+      backgroundColor: "beige",
+      borderWidth: 5,
+    },
+    bottom: {
+      flex: 0.3,
+      backgroundColor: "pink",
+      borderWidth: 5,
+      borderBottomLeftRadius: 20,
+      borderBottomRightRadius: 20,
+    },
+  });
+
