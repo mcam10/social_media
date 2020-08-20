a87f40378865f8cf9e56a25b8a371b6087e2714a
mcam1010 <malikcameron95@gmail.com>
2020-08-20 00:20:28 -0700

    adding photo/video integration for post screen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react'
-import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View , Button} from 'react-native'
 import styles from './styles';
 import { firebase } from '../../firebase/firebase_config'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { UserFeedScreen } from '../index';
 
-export default function HomeScreen() {
+export default function HomeScreen( {navigation, route }) {
 
-    const [entityText, setEntityText] = useState('')
-    const [entities, setEntities] = useState([])
-
-    const entityRef = firebase.firestore().collection('entities')
-
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Post something in order for your home screen to populate</Text>
-        </View>
-      );
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
     }
+  }, [route.params?.post]);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+    </View>
+  );
+}
--- src/screens/PostScreen/PostScreen.js
+++ src/screens/PostScreen/PostScreen.js
@@ -1,13 +1,21 @@
 import React, { useState,useEffect } from 'react'
-import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { Camera } from 'expo-camera';
+import { Image, Text, TextInput, Button, TouchableOpacity, View } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './styles';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 
 
-export default function PostScreen({navigation}) {
+export default function PostScreen({ navigation, route }){
+
+    const [postText, setPostText] = React.useState('');
+
+    const [postImage, setPostImage] = React.useState(null);
+
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.header}>
@@ -15,7 +23,13 @@ export default function PostScreen({navigation}) {
                 <Ionicons name="md-arrow-back" size={24} color="black"></Ionicons>
             </TouchableOpacity>
             <TouchableOpacity>
-                     <Text style={{ fontWeight: "900"}}>Post</Text>
+                     <Text style={{ fontWeight: "900"}} 
+                     onPress={() => {
+                            // pass params back to home sceeen
+                            navigation.navigate('Home', {post: postText});
+                            this.postText = ''
+                    }}
+                     >Post</Text>
                  </TouchableOpacity>
             </View>
             <View style={styles.Postheader}>
@@ -33,22 +47,61 @@ export default function PostScreen({navigation}) {
              multiline={true}
              numberOfLines={ 4 }
              style={{ flex: 1}}
+             value={postText}
+             onChangeText={setPostText}
              placeholder="Want to share something?"
+             blurOnSubmit = {true}
             ></TextInput>
               <View style={styles.photo}>
               </View>
                  </TouchableOpacity>
             </View>
-      
-            
         <View style={styles.container}>
-          <View style={styles.photo}>
+            <TouchableOpacity>
+            <View style={styles.photo} onPress={() => {
+                // launch camera in order to post pic or video
+            }}>
           <Ionicons name="md-camera" size={24} color="grey"></Ionicons>
-
-              </View> 
-       
+              </View>
+            </TouchableOpacity>
         </View>
      </SafeAreaView>
-      );
+    );
   }
+ 
+
+function componentDidMount() {
+      getPermissionAsync();
+}
+
+const getPermissionAsync = async () => {
+        if (Constants.platform.ios) {
+          const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+          if (status !== 'granted') {
+            alert('Sorry, we need camera roll permissions to make this work!');
+          }
+        }
+      }
+
+
+const _pickImage = async () => {
+        try {
+          let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+          if (!result.cancelled) {
+            this.setState({ image: result.uri });
+          }
+    
+          console.log(result)
+        } catch (E) {
+          console.log(E)
+        }
+    }
+
+
+
 
